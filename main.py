f15783bd0eedab68b9b517b6346022bb2b40a094
GoodOleNothing <alexgog77777@gmail.com>
2023-02-20 03:53:18 +0500

    begining

---
+++ main.py
@@ -0,0 +1,7 @@
+class Goods:
+    price_with_discount = 0.85
+
+    def __init__(self, item_name, item_price, amount):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.amount = amount
