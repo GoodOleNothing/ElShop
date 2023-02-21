bcc9ae2006c75eaf7deef989c825624b3b252d8f
GoodOleNothing <alexgog77777@gmail.com>
2023-02-21 13:20:20 +0500

    new atr

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 class Item:
     pay_rate = 0.85
+    examples_data = []
     examples = []
 
     def __init__(self, item_name, item_price, amount):
         self.item_name = item_name
         self.item_price = item_price
         self.amount = amount
-        self.examples.append([item_name, item_price, amount])
+        self.examples_data.append([item_name, item_price, amount])
 
     def calculate_total_price(self):
+        self.examples.append(self.item_price * self.amount)
         return self.item_price * self.amount
 
     def apply_discount(self):
+        self.examples.append(self.item_price * self.pay_rate)
         return self.item_price * self.pay_rate
 
 
@@ -24,5 +27,6 @@ print(item2.calculate_total_price())
 Item.pay_rate = 0.8
 
 print(item1.apply_discount())
-#print(item2.item_price)
+print(item2.item_price)
+print(item1.examples_data)
 print(item1.examples)
