56ca603a0b4f650421d8b16ad12953a658ec4535
GoodOleNothing <alexgog77777@gmail.com>
2023-02-25 14:05:11 +0500

    2 ot of 3

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,20 @@
 import pytest
 from main import Item
 
-#@pytest.fixture
-#def item_data():
-#    return ["Смартфон", 10000, 20]
 
 example = Item("Смартфон", 10000, 20)
+
+#def test_item_name():
+#    assert example.item_name() == None
+#
+##def test_instantiate_from_csv():
+##    assert example.instantiate_from_csv() == 1
+
 def test_calculate_total_price():
     assert example.calculate_total_price() == 200000
 
 def test_apply_discount():
-    assert example.apply_discount() == 8000.0
+    assert example.apply_discount() == 8500.0
+
+
+
