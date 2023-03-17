e6bc73ce598d57712b774700eb6ea27c575b45e3
GoodOleNothing <alexgog77777@gmail.com>
2023-03-17 20:33:20 +0500

    made new directory for classes, separated classes to moduls

--- workshop/item.py
+++ workshop/item.py
@@ -70,22 +70,3 @@ class Item:
         if not isinstance(other, Item):
             raise ValueError('Нельзя сложить Phone или Item с экземплярами не Phone или Item классов')
         return self.amount + other.amount
-
-
-class Phone(Item):
-
-    def __init__(self, item_name, item_price, amount, number_of_sim):
-        super().__init__(item_name, item_price, amount)
-        self._number_of_sim = number_of_sim
-
-    @property
-    def number_of_sim(self):
-        """Количество физических SIM-карт должно быть целым числом больше нуля."""
-        return self._number_of_sim
-
-    @number_of_sim.setter
-    def number_of_sim(self, value: int):
-        if isinstance(value, int) and int(value) > 0:
-            self._number_of_sim = value
-        else:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
